bde1329c4a589c224b36044c8ca8f53343a1628c
배성완 <baesee@kakao.com>
2023-07-09 06:41:52 +0900

    firebase setting

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -3,7 +3,21 @@ import React from "react";
 function Login() {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <label>아이디</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>비밀번호</label>
+          <input type="password" />
+        </div>
+        <div>
+          <label>Github 아이디</label>
+          <input type="text" />
+        </div>
+        <button>로그인</button>
+      </div>
     </>
   );
 }
