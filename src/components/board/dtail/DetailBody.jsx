351229f585b7eb4fd63d5c7362b1aed5dd6b51a4
배성완 <baesee@kakao.com>
2023-07-18 19:57:22 +0900

    editor : toast ui editor로 교체 DtailBody 부분도 toast ui editor에 viewer로 보여줌 아직 viewer 부분이 글씨가 작아보이는게 문제라서 css로 전체 지정 다시 해야할거 같음

--- src/components/board/dtail/DetailBody.jsx
+++ src/components/board/dtail/DetailBody.jsx
@@ -1,24 +1,20 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import parse from "html-react-parser";
+import { Viewer } from "@toast-ui/react-editor";
+// import "@toast-ui/editor/dist/toastui-editor.css";
+
 function DetailBody(props) {
-  const data = props.Data.content;
-  const innerHTML = `${data}`;
-  const options = {
-    replace: (domNode) => {
-      if (domNode.attribs && domNode.attribs.class) {
-        domNode.attribs.className = domNode.attribs.class;
-        delete domNode.attribs.class;
-      }
-      return domNode;
-    },
-  };
-  const content = parse(innerHTML, options);
-  return <DetailBodyContainer>{content}</DetailBodyContainer>;
+  const contents = props.Data.content;
+
+  return (
+    <DetailBodyContainer>
+      {contents && <Viewer initialValue={contents} />}
+    </DetailBodyContainer>
+  );
 }
 const DetailBodyContainer = styled.div`
   width: 80%;
-  height: 70vh;
   margin: 25px auto;
+  font-size: 3rem;
 `;
 export default DetailBody;
