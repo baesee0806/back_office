4a18992e71745798fb630425b484547bbe4a186f
배성완 <baesee@kakao.com>
2023-07-26 01:04:18 +0900

    board 번호 등록일 조회 데이터 board detail로 들어갈 경우 조회수 1추가

--- src/components/board/BoardBody.jsx
+++ src/components/board/BoardBody.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { doc, updateDoc } from "firebase/firestore";
+import { firestore } from "../../apis/firebaseService";
 
 function BoardBody(props) {
   const navigate = useNavigate();
   const data = props.item.data();
   const docRef = props.docRef;
+  const year = data.createdAt.toDate().getFullYear().toString();
+  const month = data.createdAt.toDate().getMonth() + 1;
+  const day = data.createdAt.toDate().getDate().toString();
+
+  const updateData = async () => {
+    const q = doc(firestore, "board", docRef);
+    const querySnapshot = await updateDoc(q, {
+      view: data.view + 1,
+    });
+  };
   return (
     <Tbody
       onClick={() => {
         navigate(`/board/${docRef}`);
+        updateData();
       }}
     >
       <Tr>
-        <Td>{"##"}</Td>
+        <Td>{data.docNumber}</Td>
         <Td>{data.title}</Td>
         <Td>{data.userName}</Td>
-        <Td>{"***"}</Td>
-        <Td>{"##"}</Td>
+        <Td>{year.slice(2) + "." + month + "." + day}</Td>
+        <Td>{data.view}</Td>
       </Tr>
     </Tbody>
   );
